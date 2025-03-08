d9f481ac3a6f3a02dc20f931797674b9b0619ed2
Mirion <mironnesvetaylov@gmail.com>
2025-03-08 19:32:16 +0300

    dap configurations

--- nvim/lua/languages/init.lua
+++ nvim/lua/languages/init.lua
@@ -12,24 +12,6 @@ vim.snippet.add = function(trig, body, opts)
   })
 end
 
----Allows you to pick any file in your current directory and return it as a string
----@async
----@return string
-vim.pick_file = function()
-  local co = coroutine.running()
-  require("fzf-lua").files({
-    cmd = "find .",
-    git_icons = false,
-    file_icons = false,
-    actions = {
-      default = function(selected)
-        coroutine.resume(co, selected[1])
-      end
-    }
-  })
-  return coroutine.yield()
-end
-
 for _, path in ipairs(vim.api.nvim_get_runtime_file("lua/languages/*.lua", true)) do
   if not vim.endswith(path, "init.lua") then
     local config = loadfile(path)()
--- nvim/lua/languages/rust.lua
+++ nvim/lua/languages/rust.lua
@@ -12,7 +12,7 @@ dap.configurations.rust = {
     name = 'Launch',
     type = 'lldb',
     request = 'launch',
-    program = vim.pick_file,
+    program = require("dap.utils").pick_file,
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
     args = {},
@@ -31,5 +31,14 @@ dap.configurations.rust = {
         ([[command source '%s']]):format(commands_file),
       }
     end,
+  },
+  {
+    -- If you get an "Operation not permitted" error using this, try disabling YAMA:
+    --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    name = "Attach to process",
+    type = 'lldb',   -- Adjust this to match your adapter name (`dap.adapters.<name>`)
+    request = 'attach',
+    pid =  require("dap.utils").pick_process,
+    args = {},
   },
 }
