3e3f1de147261da37f8996803086c68777d46e29
Mirion <mironnesvetaylov@gmail.com>
2025-03-19 18:59:10 +0300

    small tweaks, add lualine

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -2,12 +2,11 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      "LiadOz/nvim-dap-repl-highlights",
+      "jbyuki/one-small-step-for-vimkind",
     },
     lazy = true,
     config = function()
       local dap = require("dap")
-      require('nvim-dap-repl-highlights').setup()
       vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'red' })
       vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = 'blue' })
       vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'orange' })
