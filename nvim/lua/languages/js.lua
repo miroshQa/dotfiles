d091a4a3e2f0224c8311c4fc4be4b0c804214d94
Mirion <mironnesvetaylov@gmail.com>
2025-03-23 20:54:50 +0300

    new js snippets

--- nvim/lua/languages/js.lua
+++ nvim/lua/languages/js.lua
@@ -1,7 +1,20 @@
 vim.snippet.add("at", "/** @type {${1:Type}} */\n$0", {ft = "javascript"})
 vim.snippet.add("ai", "/** @implements {${1:Interface}} */\n$0", {ft = "javascript"})
 vim.snippet.add("ad", "/** @typedef {${0:Typename}} */\n$0", {ft = "javascript"})
-vim.snippet.add("lg", 'console.log("$1")', {ft = "javascript"})
+vim.snippet.add("lg", 'console.log($1)', {ft = "javascript"})
+
+vim.snippet.add("fu", [[
+function ${2:funcName}($3) {
+  $4
+}
+]], {ft = "javascript"})
+
+
+vim.snippet.add("fo", [[
+for (let ${2:value} of ${3:collection}) {
+  $4
+}
+]], {ft = "javascript"})
 
 vim.dap.adapters["pwa-node"] = {
   type = "server",
