fe6a7224c2fbfeb336b0707d30c68bf16e444444
Mirion <mironnesvetaylov@gmail.com>
2025-04-02 23:09:38 +0300

    add treesitter text objects back

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,40 @@ return {
       })
     end,
   },
-  -- Automatically add closing tags for HTML and JSX
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.configs").setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+            },
+            include_surrounding_whitespace = true,
+          },
+          swap = {
+            enable = true,
+            swap_next = { ["}"] = "@parameter.inner", },
+            swap_previous = { ["{"] = "@parameter.inner", },
+          },
+          move = {
+            enable = true,
+            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+          },
+        },
+      }
+    end
+  },
   {
     "windwp/nvim-ts-autotag",
     ft = { "js", "html", "javascriptreact", "ts", "jsx", "tsx" },
