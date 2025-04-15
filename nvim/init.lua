80c8591589e4757090eac4d690570d21b2274ac1
Mirion <mironnesvetaylov@gmail.com>
2025-04-15 21:13:58 +0300

    feat: persistent colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,16 @@ require("core.autocommands")
 require("core.pluginmanager")
 require("core.keymaps")
 require("languages")
-vim.cmd("colorscheme gruvboxbaby")
+
+local _ = (function()
+  local default = "gruvboxbaby"
+  local path = vim.fs.joinpath(vim.fn.stdpath("cache"), "/colorscheme")
+  local file = io.open(path, "r")
+  local colorscheme = file and file:read("*L") or default
+  vim.cmd("colorscheme " ..  colorscheme)
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function(args)
+      io.open(path, "w+"):write(args.match)
+    end
+  })
+end)()
