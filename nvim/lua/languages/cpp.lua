e8bb9df52e4def3892626b0c7e4ff42989122060
Mirion <mironnesvetaylov@gmail.com>
2025-04-18 15:42:27 +0300

    remove all adapter configurations except lldb-dap

--- nvim/lua/languages/cpp.lua
+++ nvim/lua/languages/cpp.lua
@@ -1,40 +1,23 @@
-vim.dap.adapters.cppdbg = {
-  id = 'cppdbg',
-  type = 'executable',
-  command = vim.get_mason_bin('OpenDebugAD7'),
-}
-
 vim.dap.configurations.cpp = {
   {
     name = 'Launch file (lldb-dap)',
     type = 'lldb',
     request = 'launch',
-    program = vim.dap.utils.pick_file,
+    program = "${command:pickFile}",
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
     runInTerminal = true,
-  },
-  {
-    name = "Launch file (cpptools)",
-    type = "cppdbg",
-    request = "launch",
-    program = "${command:pickFile}",
-    cwd = '${workspaceFolder}',
-    setupCommands = {
-      {
-        text = '-enable-pretty-printing',
-        description = 'enable pretty printing',
-        ignoreFailures = false
-      },
-    },
+    initCommands = {
+      "process handle SIGWINCH -p true -s false -n false"
+    }
   },
   {
     -- If you get an "Operation not permitted" error using this, try disabling YAMA:
     --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    name = "Attach to process",
+    name = "Attach to process (lldb-dap)",
     type = 'lldb', -- Adjust this to match your adapter name (`dap.adapters.<name>`)
     request = 'attach',
-    pid = vim.dap.utils.pick_process,
+    pid = "${command:pickProcess}",
     args = {},
   },
 }
